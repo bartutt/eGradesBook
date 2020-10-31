306670324129381ec35d775a0fab0d33bec59bc6
bartutt <witkowski.bartlomiej1@gmail.com>
2020-10-31 14:14:38 +0100

    controller updated

--- admin/functions/class.controller.php
+++ admin/functions/class.controller.php
@@ -1,29 +1,41 @@
 <?php 
-require_once './functions/class.database.php';
-require_once './functions/class.random_person.php';
 require_once './functions/variables.php';
 
 class Controller {
 
     private $database;
 
-    function __construct() {
+    function __construct($database) {
 
-        $this->database = new DataBase;
+        $this->database = $database;
 
     }
  
-    public function handleRequest ($action, $value) {
+    
+    public function handleRequest ($action, $old_value = '', $value) {
 
         switch ($action) {
          
             case 'add_year':
                 $this->database->addYear($value);
+                $this->database->getErrors();
+                $this->database->getSuccess();
                 break;
 
             case 'set_year':  
-
+                $this->database->setYear($value);
+                $this->database->getErrors();
+                $this->database->getSuccess();
                 break;
+            
+            case 'set_lesson_time':  
+                $this->database->setLessonTime($old_value, $value);
+                $this->database->getErrors();
+                $this->database->getSuccess();
+                break;
+
+
+
             }
 
     }
