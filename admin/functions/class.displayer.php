1a1707e0590cd2d31ebee7e4c5d1883ac860e765
bartutt <witkowski.bartlomiej1@gmail.com>
2020-10-31 14:15:28 +0100

    alarms from db ref

--- admin/functions/class.displayer.php
+++ admin/functions/class.displayer.php
@@ -1,79 +1,148 @@
 <?php 
-require_once './functions/class.database.php';
 
 class Displayer{
 
     private $database;
 
-    function __construct() {
+    function __construct($database) {
 
-        $this->database = new DataBase;
+        $this->database = $database;
 
     }
  
-
+     
+  public function displayErrors(){
+     if (!empty ($this->database->getErrors() ) )
+      foreach ($this->database->getErrors() as $error) {
+            echo '
+            <div class = "container">
+            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <h4 class="alert-heading">Error</h4><hr>' . $error .
+                '<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            </div>';
+        }          
     
-    public function displayYearsSelect() {
+    }
 
-        if (!empty ($this->database->years)){
-            foreach ($this->database->years as $year)
+  public function displaySuccess(){
+      if (!empty ($this->database->getSuccess() ) )
+       foreach ($this->database->getSuccess() as $success) {
+             echo '
+             <div class = "container">
+             <div class="alert alert-success alert-dismissible fade show" role="alert">
+             <h4 class="alert-heading">Success!</h4><hr>' . $success .
+                 '<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                 <span aria-hidden="true">&times;</span>
+                 </button>
+             </div>
+             </div>';
+         }          
+     
+    }
+    
+  public function displayYearsSelect() {
+
+            foreach ($this->database->getYears() as $year)
                 echo 
                 '<option value = ' . $year .'>'         
                      . $year .                       
                 '</option>';
-        }else 
-            $this->error[] = '0 results';
+    
     
     }
 
- public function displayLessonTimes() {
+  public function displayLessonTimesEdit() {
+    $i = 1;
+      foreach ($this->database->getLessonTimes() as $lesson){
+          echo '
+          <form action = "settings.php" method = "post">
+              <div class="col-12">
+                <button class="button" data-toggle="modal" data-target="#lesson'. $i .'">
+                  ' . $lesson .'
+                </button>
 
-        if (!empty ($this->database->lesson_times)){
-            foreach ($this->database->lesson_times as $lesson)
-                echo '
-                <div class="table-responsive">
-                    <table class="table">
-                        <tr>
-                            <td> ' . $lesson .'</td>
-                            <td>
+                <div class="modal fade" id="lesson' . $i . '" tabindex="-1" role="dialog" aria-labelledby="lesson' .$i . '" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                          
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="lesson' . $i . '">New time</h5>
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                          </button>
+                      </div>
 
-                            <button type="button" class="button" data-toggle="modal" data-target="#exampleModal">
-                              edit
+                      <div class="modal-body">          
+                        <div class="form-group">
+                          <input type = "text" class="form-control" name = "value" placeholder = "' . $lesson . '" required>
+                            <input type = "hidden" name = "action" value = "set_lesson_time">
+                            <input name = "old_value" type = "hidden" value = "' . $lesson . '">
+                        </div>
+                      </div>
+
+                      <div class="modal-footer">
+                        <button type="button" class="button" data-dismiss="modal">Cancel</button>
+                        <button type="submit" class="button">Save changes</button>
+                      </div>
+                        
+                    </div>
+                  </div>
+                </div>            
+              </div>
+          </form>';
+        $i++;
+      }// end foreach loop
+    } // end of method
+
+  
+  public function displayMarksCat() {
+      $i = 1;
+        foreach ($this->database->getMarksCat() as $marks_cat){
+            echo '
+            <form action = "settings.php" method = "post">
+                <div class="col-12">
+                  <button class="button" data-toggle="modal" data-target="#lesson'. $i .'">
+                    ' . $marks_cat .'
+                  </button>
+  
+                  <div class="modal fade" id="lesson' . $i . '" tabindex="-1" role="dialog" aria-labelledby="lesson' .$i . '" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                      <div class="modal-content">
+                            
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="lesson' . $i . '">New time</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
                             </button>
-
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                              <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                  <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">New time</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                      <span aria-hidden="true">&times;</span>
-                                    </button>
-                                  </div>
-                                  <div class="modal-body">
-                                    
-                                <div class="form-group">
-                                <input  type = "text" class="form-control" name = "year" form = "add" placeholder = "xx.xx-xx.xx" required>
-                                </div>
-
-
-                                  </div>
-                                  <div class="modal-footer">
-                                    <button type="button" class="button" data-dismiss="modal">Cancel</button>
-                                    <button type="button" class="button">Save changes</button>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                            
-                            
-                            </td>                         
-                        </tr>
-                    </table>
-                </div>';
-        }else 
-            $this->error[] = '0 results';
-    
-    }
-}
+                        </div>
+  
+                        <div class="modal-body">          
+                          <div class="form-group">
+                            <input type = "text" class="form-control" name = "value" placeholder = "' . $marks_cat . '" required>
+                              <input type = "hidden" name = "action" value = "set_lesson_time">
+                              <input name = "old_value" type = "hidden" value = "' . $marks_cat . '">
+                          </div>
+                        </div>
+  
+                        <div class="modal-footer">
+                          <button type="button" class="button" data-dismiss="modal">Cancel</button>
+                          <button type="submit" class="button">Save changes</button>
+                        </div>
+                          
+                      </div>
+                    </div>
+                  </div>            
+                </div>
+            </form>';
+          $i++;
+        }// end foreach loop
+      }//end of method
+  
+  
+  
+  
+  }// end of class
 ?>
