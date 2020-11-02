f86c840c4ec4a8bfd0a5f07d6fef7039075ee5e3
bartutt <witkowski.bartlomiej1@gmail.com>
2020-11-02 13:16:39 +0100

    display as button method

--- admin/functions/class.displayer.php
+++ admin/functions/class.displayer.php
@@ -54,95 +54,51 @@ class Displayer{
     
     }
 
-  public function displayLessonTimesEdit() {
+  public function displayContentAsButton($source, $as, $id, $action_value){
     $i = 1;
-      foreach ($this->database->getLessonTimes() as $lesson){
-          echo '
-          <form action = "settings.php" method = "post">
-              <div class="col-12">
-                <button class="button" data-toggle="modal" data-target="#lesson'. $i .'">
-                  ' . $lesson .'
-                </button>
+    foreach ($this->database->$source() as $as){
+        echo '
+        <form action = "settings.php" method = "post">
+            <div class="col-12">
+              <button class="button" data-toggle="modal" data-target="#'. $id. $i .'" >
+                ' . $as .'
+              </button>
 
-                <div class="modal fade" id="lesson' . $i . '" tabindex="-1" role="dialog" aria-labelledby="lesson' .$i . '" aria-hidden="true">
-                  <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                          
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="lesson' . $i . '">New time</h5>
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                      </div>
-
-                      <div class="modal-body">          
-                        <div class="form-group">
-                          <input type = "text" class="form-control" name = "value" placeholder = "' . $lesson . '" required>
-                            <input type = "hidden" name = "action" value = "set_lesson_time">
-                            <input name = "old_value" type = "hidden" value = "' . $lesson . '">
-                        </div>
-                      </div>
-
-                      <div class="modal-footer">
-                        <button type="button" class="button" data-dismiss="modal">Cancel</button>
-                        <button type="submit" class="button">Save changes</button>
-                      </div>
+              <div class="modal fade" id="'. $id . $i . '" tabindex="-1" role="dialog" aria-labelledby="'. $id . $i . '" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
                         
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="'. $id . $i . '">Edit</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
-                  </div>
-                </div>            
-              </div>
-          </form>';
-        $i++;
-      }// end foreach loop
-    } // end of method
 
-  
-  public function displayMarksCat() {
-      $i = 1;
-        foreach ($this->database->getMarksCat() as $marks_cat){
-            echo '
-            <form action = "settings.php" method = "post">
-                <div class="col-12">
-                  <button class="button" data-toggle="modal" data-target="#mark'. $i .'">
-                    ' . $marks_cat .'
-                  </button>
-  
-                  <div class="modal fade" id="mark' . $i . '" tabindex="-1" role="dialog" aria-labelledby="mark' .$i . '" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                      <div class="modal-content">
-                            
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="mark' . $i . '">Edit</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-  
-                        <div class="modal-body">          
-                          <div class="form-group">
-                            <input type = "text" class="form-control" name = "value" placeholder = "' . $marks_cat . '" required>
-                              <input type = "hidden" name = "action" value = "set_mark_cat">
-                              <input name = "old_value" type = "hidden" value = "' . $marks_cat . '">
-                          </div>
-                        </div>
-  
-                        <div class="modal-footer">
-                          <button type="button" class="button" data-dismiss="modal">Cancel</button>
-                          <button type="submit" class="button">Save changes</button>
-                        </div>
-                          
+                    <div class="modal-body">          
+                      <div class="form-group">
+                        <input type = "text" class="form-control" name = "value" placeholder = "'. $as . '" required>
+                          <input type = "hidden" name = "action" value = "' . $action_value . '">
+                          <input name = "old_value" type = "hidden" value = "' . $as . '">
                       </div>
                     </div>
-                  </div>            
+
+                    <div class="modal-footer">
+                      <button type="button" class="button" data-dismiss="modal">Cancel</button>
+                      <button type="submit" class="button">Save changes</button>
+                    </div>
+                      
+                  </div>
                 </div>
-            </form>';
-          $i++;
-        }// end foreach loop
-      }//end of method
-  
-  
-  
-  
+              </div>            
+            </div>
+        </form>';
+      $i++;
+    }
+  }
+
+
+
+
   }// end of class
 ?>
